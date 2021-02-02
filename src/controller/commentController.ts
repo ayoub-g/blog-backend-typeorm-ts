36021bc7c82e83e84fea394617b425b2fdf5212d
Ayoub.Gharbi <gharbi.ayoub@gmail.com>
2021-02-02 12:39:50 +0100

    added update and delete comment

--- src/controller/commentController.ts
+++ src/controller/commentController.ts
@@ -10,6 +10,8 @@ export class CommentController {
     constructor(commentRespository) {
         this.router = express.Router();
         this.commentRepository = commentRespository;
+        this.router.delete("/", this.deleteComment);
+        this.router.patch("/", this.updateComment);
         this.router.post('/', this.addComment);
         this.router.get('/', this.getPostComments);
     }
@@ -66,6 +68,53 @@ export class CommentController {
             res
                 .status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR)
                 .json({ message: 'error' });
+        }
+    };
+
+    updateComment = async (req, res) => {
+        const comment = req.body;
+        try {
+            if (comment && comment.content && comment.id && comment.date) {
+                await this.commentRepository
+                    .createQueryBuilder()
+                    .update(Comment)
+                    .set(comment)
+                    .where("id = :id", { id: comment.id })
+                    .execute();
+                res.status(HttpStatus.StatusCodes.CREATED).json({ message: "OK" });
+            } else {
+                res
+                    .status(HttpStatus.StatusCodes.BAD_REQUEST)
+                    .json({ message: "missing query parameters" });
+            }
+        } catch (error) {
+            // console.log(error);
+            res
+                .status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR)
+                .json({ message: "error" });
+        }
+    };
+    deleteComment = async (req, res) => {
+        try {
+            const { id } = url.parse(req.url, true).query;
+            if (id) {
+                await this.commentRepository
+                    .createQueryBuilder()
+                    .delete()
+                    .from(Comment)
+                    .where("id = :id", { id })
+                    .execute();
+                res.status(HttpStatus.StatusCodes.CREATED).json({ message: "OK" });
+            } else {
+                res
+                    .status(HttpStatus.StatusCodes.BAD_REQUEST)
+                    .json({ message: "missing query parameters" });
+            }
+        } catch (error) {
+            // console.log(error);
+            res
+                .status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR)
+                .json({ message: "error" });
         }
     };
 };
